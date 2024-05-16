6250448adce7ebb50f6b14bc52e80badbe01201c
Erin <50000677+sinclairems@users.noreply.github.com>
2024-05-16 17:51:51 -0500

    chore: Update npm dependencies and add react-router-dom

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-export default {
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom'],
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-};
+  build: {
+    rollupOptions: {
+      external: ["react-router-dom"],
+    },
+  },
+  server: {
+    port: 5501,
+    open: true,
+  },
+});
